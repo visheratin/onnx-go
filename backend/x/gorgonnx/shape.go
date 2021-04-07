439711e22d16f3f54d1e8061cd3d02430df86452
Alexander Visheratin <alexvish91@gmail.com>
2021-04-07 00:25:02 -0400

    Added graph to the Shape operator.

--- backend/x/gorgonnx/shape.go
+++ backend/x/gorgonnx/shape.go
@@ -2,6 +2,7 @@ package gorgonnx
 
 import (
 	"errors"
+
 	"github.com/owulveryck/onnx-go"
 	"gorgonia.org/gorgonia"
 	"gorgonia.org/tensor"
@@ -23,8 +24,12 @@ func (*shape) apply(graph *Graph, nodes ...*Node) error {
 		return err
 	}
 	s := []int(children[0].gorgoniaNode.Shape())
-	t := tensor.New(tensor.WithShape(len(s)), tensor.WithBacking(s))
-	nodes[0].gorgoniaNode = gorgonia.NewConstant(t)
+	v := make([]int64, len(s))
+	for i := range s {
+		v[i] = int64(s[i])
+	}
+	t := tensor.New(tensor.WithShape(len(s)), tensor.WithBacking(v))
+	nodes[0].gorgoniaNode = gorgonia.NewTensor(graph.exprgraph, t.Dtype(), t.Dims(), gorgonia.WithValue(t))
 
 	return nil
 }
